356da38862247ccec592b5abdec847a29c8ec899
Robert-Andrei OPRESCU <robert.oprescu@stud.aero.upb.ro>
2021-10-30 20:43:07 +0300

    added navbar button

--- viralize-app/src/app/landing-page/landing-page.component.ts
+++ viralize-app/src/app/landing-page/landing-page.component.ts
@@ -10,6 +10,12 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // let navbartoggler = document.getElementById('navbar-toggler');
+    // navbartoggler!.addEventListener('click', (event) => navbartoggler!.style.border = "none");
+
+
+
   }
 
 }
