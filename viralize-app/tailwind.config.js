e88e9cd34c02495ab56c4e05853c2621333f36eb
Robert-Andrei OPRESCU <robert.oprescu@stud.aero.upb.ro>
2022-01-09 21:32:43 +0200

    intalled tailwindcss

---
+++ viralize-app/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/app/dashboard/dashboard.component.html"],
+  // purge: [],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
